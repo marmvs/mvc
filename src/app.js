aaa0c49890ec5950737226dd663c54c6d99d0fed
marmvs <marcia2005vs@hotmail.com>
2023-06-21 17:11:52 -0300

    atualizando

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import index from './routes/index.js'
+
+
+const app = express();  //esta variável recebe express e sera utilizada em outros arquivos
+
+app.use(express.json());
+app.use("/", index);
+
+export default app
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const router = express.Router;
+
+router.length("/", function (req, res) {
+    res.status(200).send({
+        title: "Bem vindas a turma Devas do Full-Stack",
+        version: "1.0.0"
+    })
+})
+
+export default router;
